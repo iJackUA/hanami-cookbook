f119da6010ee7b63cac9147717a8f410c2ae8847
Yevhen Kuzminov <kyzminov@gmail.com>
2018-07-22 21:50:34 +0300

    add sources widget

--- .vuepress/theme-backup/Layout.vue
+++ .vuepress/theme-backup/Layout.vue
@@ -1,7 +1,15 @@
 <template>
     <Layout>
       <template slot="page-bottom">
-        <!-- <h1>Related receipts</h1> -->
+
+         <h2>Sources: </h2>
+          <div class="info-sources" v-for="source in $page.frontmatter.sources">
+            <a :href="source">{{ source }}</a>
+          </div>
+
+         <h2>Related receipts</h2>
+
+         
       </template>
     </Layout>
 </template>
